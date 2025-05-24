9cc3d390df0c1608069a99979472d9c1c7be376a
oumaoumag <ouma.godwin10@gmail.com>
2025-05-25 00:39:44 +0300

    Refactor App component to implement routing and replace previous layout with new page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,26 @@
-import { ConnectButton, useCurrentAccount } from "@mysten/dapp-kit";
-import { isValidSuiObjectId } from "@mysten/sui/utils";
-import { Box, Container, Flex, Heading } from "@radix-ui/themes";
-import { useState } from "react";
-import { Counter } from "./Counter";
-import { CreateCounter } from "./CreateCounter";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import EventDiscovery from './pages/EventDiscovery';
+import BadgeMinting from './pages/BadgeMinting';
+import UserProfile from './pages/UserProfile';
+import Marketplace from './pages/Marketplace';
+import OrganizerPortal from './pages/OrganizerPortal';
 
 function App() {
-  const currentAccount = useCurrentAccount();
-  const [counterId, setCounter] = useState(() => {
-    const hash = window.location.hash.slice(1);
-    return isValidSuiObjectId(hash) ? hash : null;
-  });
-
-  return (
-    <>
-      <Flex
-        position="sticky"
-        px="4"
-        py="2"
-        justify="between"
-        style={{
-          borderBottom: "1px solid var(--gray-a2)",
-        }}
-      >
-        <Box>
-          <Heading>dApp Starter Template</Heading>
-        </Box>
-
-        <Box>
-          <ConnectButton />
-        </Box>
-      </Flex>
-      <Container>
-        <Container
-          mt="5"
-          pt="2"
-          px="4"
-          style={{ background: "var(--gray-a2)", minHeight: 500 }}
-        >
-          {currentAccount ? (
-            counterId ? (
-              <Counter id={counterId} />
-            ) : (
-              <CreateCounter
-                onCreated={(id) => {
-                  window.location.hash = id;
-                  setCounter(id);
-                }}
-              />
-            )
-          ) : (
-            <Heading>Please connect your wallet</Heading>
-          )}
-        </Container>
-      </Container>
-    </>
+   return (
+    <BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<EventDiscovery />} />
+        <Route path="/mint" element={<BadgeMinting />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/marketplace" element={<Marketplace />} />
+        <Route path="/organizer" element={<OrganizerPortal />} />
+      </Routes>
+    </Layout>
+    </BrowserRouter>     
   );
 }
 
