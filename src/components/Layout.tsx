720b3b9b1c5aaf98923f9260051113c0214dbe4f
oumaoumag <ouma.godwin10@gmail.com>
2025-05-25 00:46:18 +0300

    Fix typo in Layout component props and ensure proper rendering structure

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,3 +2,15 @@ import { Flex, Heading } from '@radix-ui/themes';
 import { ConnectButton } from '@mystem/dapp-kit';
 import { Link } from 'react-router-dom';
 import '../styles/global.css'; 
+
+function Layout({ childreb }: { children: React.ReactNode }) {
+    return (
+        <div className="layout">
+            <header>
+                <Flex justify="between" align="center" p="4" className="header">
+                    <Heading as="h1" size="8">SuiMemento</Heading>
+                </Flex>
+            </header>
+        </div>
+    )
+}
