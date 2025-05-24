eab5c4b7d59baeeb81a6ce9bb28121bbe18882d4
oumaoumag <ouma.godwin10@gmail.com>
2025-05-25 01:15:48 +0300

    Refactor Home component to properly define and utilize mock event data for the slider

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,28 @@ function Home() {
         autoplay: true,
     };
 
+
     // Mock event data // To be replaced with blockchain data letter
-    { id: 1, name: "AfroTech Summit", data: "2025-03-15"},
-    { id: 2, name: "Nairobi Music Fest", date: "2025-04-10"},
+    const events = [ 
+        { id: 1, name: "AfroTech Summit", date: "2025-03-15"},
+        { id: 2, name: "Nairobi Music Fest", date: "2025-04-10"},
+    ];
+
+    return (
+        <div className='page home'>
+            <Heading size="9">Welcome to SuiMemento</Heading>
+            <Text size='5'>Revolutionizing Event Engagement with NFT Badges</Text>
+            <Slider {...settings}>
+                {events.map(event => (
+                    <div key={event.id}>
+                        <Heading>{event.name}</Heading>
+                        <Text>Date: {event.date}</Text>
+                    </div>
+                ))}
+            </Slider>
+            {/* TODO: Add testimonials and highlights */}
+        </div>
+    );
 }
+
+export default Home;
