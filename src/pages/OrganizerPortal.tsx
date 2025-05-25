4a7506435c1d95e028d698f11d0fb515e9c5cbba
oumaoumag <ouma.godwin10@gmail.com>
2025-05-25 07:19:38 +0300

    Add initial state management to OrganizerPortal component

--- src/pages/OrganizerPortal.tsx
+++ src/pages/OrganizerPortal.tsx
@@ -3,3 +3,11 @@ import { Heading, Flex, Text, Button, TextField } from '@radix-ui/themes';
 import { Transaction } from '@mysten/sui/transactions';
 import { useSignAndExecuteTransaction } from '@mysten/dapp-kit';
 import { TESTNET_EVENT_PACKAGE_ID } from '../constants';
+
+function OrganizerPortal() {
+    const [eventName, setEventName] = useState('');
+    const [date, setDate] = useState('');
+    const [location, setLocation] = useState('');
+    const [description, setDescription] = useState('');
+    const [badgeDesign, setBadgeDesign] = useState('');
+    const { mutate: signAndExecute, isPending } = useSignAndExecuteTransaction();
