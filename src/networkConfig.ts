3b43a35b8391b075a5c8222602e7c40bcddce642
oumaoumag <ouma.godwin10@gmail.com>
2025-05-25 07:05:29 +0300

    Refactor network configuration to remove unused devnet and mainnet settings

--- src/networkConfig.ts
+++ src/networkConfig.ts
@@ -1,31 +1,33 @@
 import { getFullnodeUrl } from "@mysten/sui/client";
 import {
-  DEVNET_COUNTER_PACKAGE_ID,
-  TESTNET_COUNTER_PACKAGE_ID,
-  MAINNET_COUNTER_PACKAGE_ID,
+  TESTNET_EVENT_PACKAGE_ID,
+  TESTNET_BADGE_PACKAGE_ID,
+  TESTNET_MARKETPLACE_PACKAGE_ID,
 } from "./constants.ts";
 import { createNetworkConfig } from "@mysten/dapp-kit";
 
 const { networkConfig, useNetworkVariable, useNetworkVariables } =
   createNetworkConfig({
-    devnet: {
-      url: getFullnodeUrl("devnet"),
-      variables: {
-        counterPackageId: DEVNET_COUNTER_PACKAGE_ID,
-      },
-    },
+    // devnet: {
+    //   url: getFullnodeUrl("devnet"),
+    //   variables: {
+    //     counterPackageId: DEVNET_COUNTER_PACKAGE_ID,
+    //   },
+    // },
     testnet: {
       url: getFullnodeUrl("testnet"),
       variables: {
-        counterPackageId: TESTNET_COUNTER_PACKAGE_ID,
+        eventPackageId: TESTNET_EVENT_PACKAGE_ID,
+        badgePackageId: TESTNET_BADGE_PACKAGE_ID,
+        marketplace: TESTNET_MARKETPLACE_PACKAGE_ID,
       },
     },
-    mainnet: {
-      url: getFullnodeUrl("mainnet"),
-      variables: {
-        counterPackageId: MAINNET_COUNTER_PACKAGE_ID,
-      },
-    },
+  //   mainnet: {
+  //     url: getFullnodeUrl("mainnet"),
+  //     variables: {
+  //       counterPackageId: MAINNET_COUNTER_PACKAGE_ID,
+  //     },
+  //   },
   });
 
 export { useNetworkVariable, useNetworkVariables, networkConfig };
